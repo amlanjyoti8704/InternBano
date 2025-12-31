aabd79748d034fb24060f5cba7277fdc67340119
amlanjyoti8704 <162537690+amlanjyoti8704@users.noreply.github.com>
2025-12-31 00:37:04 +0000

    fix: update text color for job description in dark mode

--- InternBano/src/components/jobCard.jsx
+++ InternBano/src/components/jobCard.jsx
@@ -14,7 +14,7 @@ function JobCard({job}) {
                 <span className='bg-blue-50 dark:bg-blue-300 border border-blue-200 dark:border-blue-400 px-4 py-1.5 rounded dark:text-black'>{job.location}</span>
                 <span className='bg-red-50 dark:bg-red-300 border border-red-200 dark:border-red-400 px-4 py-1.5 rounded dark:text-black'>{job.level}</span>
             </div>
-            <p className='text-gray-500 dark:text-gray-100 text-sm mt-4' dangerouslySetInnerHTML={{__html:job.description.slice(0,150)}}></p>
+            <p className='text-gray-500 dark:text-white text-sm mt-4' dangerouslySetInnerHTML={{__html:job.description.slice(0,150)}}></p>
             <div className='mt-4 flex gap-4 text-sm'>
                 <button onClick={()=>{navigate(`/applyJobs/${job._id}`); scrollTo(0,0)}} className='bg-blue-400 text-white px-4 py-2 rounded'>Apply Now</button>
                 <button onClick={()=>{navigate(`/applyJobs/${job._id}`); scrollTo(0,0)}} className='text-gray-500 dark:text-gray-200 border border-gray-500 dark:border-gray-400 rounded px-4 py-2'>Learn More</button>
